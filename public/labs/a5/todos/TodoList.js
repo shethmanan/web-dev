5845a79623c1406025162c454b6ed03f4cb8c6d7
shethmanan <shethmanan1997@gmail.com>
2022-02-28 16:51:36 -0500

    lab cone

--- public/labs/a5/todos/TodoList.js
+++ public/labs/a5/todos/TodoList.js
@@ -1,9 +1,13 @@
+import TodoItem from "./TodoItem.js";
+import todos from "./todos.js";
 const TodoList = () => {
     return(`
         <ul>
-            <li>Buy milk</li>
-            <li>Pickup the kids</li>
-            <li>Walk the dog</li>
+             ${
+        todos.map(todo => {
+            return(TodoItem(todo));
+        }).join('')
+    }
         </ul>
     `);
 }
