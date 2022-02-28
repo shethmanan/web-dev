df14d98baadd4f6617f0b5bd77afbed0771f1530
shethmanan <shethmanan1997@gmail.com>
2022-02-27 22:38:08 -0500

    Lab incomplete

---
+++ public/labs/a5/todos/TodoList.js
@@ -0,0 +1,11 @@
+const TodoList = () => {
+    return(`
+        <ul>
+            <li>Buy milk</li>
+            <li>Pickup the kids</li>
+            <li>Walk the dog</li>
+        </ul>
+    `);
+}
+export default TodoList;
+
