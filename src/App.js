bb97a186f998192658b7a51f17f920b348edf5b0
shethmanan <shethmanan1997@gmail.com>
2022-03-21 12:32:40 -0400

    add local bootstrap and fontawesome

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hey there champ, have a good day!!
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div className="container">
+                <h1>Hello World!</h1>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
