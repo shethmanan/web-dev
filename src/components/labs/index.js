51db8bdab4dfb385ac9ba9005abfbc05a24aed32
shethmanan <shethmanan1997@gmail.com>
2022-03-27 11:44:37 -0400

    removed unused import

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -1,19 +1,11 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import Classes from "./classes";
-import Styles from "./styles";
-import ConditionalOutput from "./conditional-output";
-import TodoList from "./todo/TodoList";
 import ReduxExamples from "./redux-examples";
 const Labs = () => {
     return (
         <>
             <h1>Labs</h1>
             <ReduxExamples/>
-            {/*<TodoList/>*/}
-            {/*<Styles/>*/}
-            {/*<Classes/>*/}
-            {/*<ConditionalOutput/>*/}
 
             <Link to="/hello">
                 Hello
