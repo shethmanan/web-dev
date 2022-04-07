e7529210e9ece90a12929cee4089afee6287701a
shethmanan <shethmanan1997@gmail.com>
2022-04-06 20:51:44 -0400

    a8 completed

--- src/components/tuiter/reducers/tuits-reducer.js
+++ src/components/tuiter/reducers/tuits-reducer.js
@@ -1,43 +1,20 @@
-import tuits from "../data/tuits.json";
-
-const tuitsReducer = (state = tuits, action) => {
+import {CREATE_TUIT, FIND_ALL_TUITS, UPDATE_TUIT, DELETE_TUIT} from "./actions/tuits-actions"
+const tuitsReducer = (state = [], action) => {
     switch (action.type){
-        case "create-tuit":
-            const newTuitObject = {
-                _id: new Date().getDay() + "",
-                tuit: action.tuit,
-                topic: "Web Development",
-                handle: "ReactJS",
-                time: "now",
-                postedBy: {
-                    username: "ReactJS"
-                },
-                stats: {
-                    comments: 111,
-                    retuits: 222,
-                    likes: 333
-                },
-                'logo-image': "../tuiter/images/react.png",
-                'avatar-image': "../tuiter/images/react.png",
-            }
-            return [newTuitObject,...state];
-        case "delete-tuit":
-            return state.filter(tuit => tuit._id !== action.tuit._id);
-        case "like-tuit":
-            return state.map((tuit) =>{
-                if(tuit._id === action.tuit._id){
-                    if(action.tuit.liked){
-                        tuit.liked = false;
-                        tuit.stats.likes--;
-                    }else{
-                        tuit.liked = true;
-                        tuit.stats.likes++;
-                    }
-                }
-                return tuit;
-            })
+        case FIND_ALL_TUITS:
+            return action.tuitList;
+        case DELETE_TUIT:
+            return state.filter(
+                tuit => tuit._id !== action.tuit._id);
+        case CREATE_TUIT:
+            return [action.tuit, ...state];
+        case UPDATE_TUIT:
+            return state.map(tuit => {
+                if(tuit._id === action.tuit._id) return action.tuit;
+                else return tuit;
+            });
         default:
-            return tuits;
+            return state;
     }
 
 }
--- src/components/tuiter/tuit-list/index.js
+++ src/components/tuiter/tuit-list/index.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useEffect} from "react";
 import TuitItem from "./TuitItem";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {findAllTuits} from "../reducers/actions/tuits-actions";
+
 const TuitList = () => {
     const TuitList = useSelector(({tuitList}) => tuitList);
-    return(
+    const dispatch = useDispatch();
+
+    useEffect(() => findAllTuits(dispatch), []);
+
+
+    return (
         <ul className="list-group">
             {
                 TuitList.map(tuit => {
