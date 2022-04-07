d2250c3eb7e8340eb022bb3398de94c2c2f3efda
shethmanan <shethmanan1997@gmail.com>
2022-04-06 22:16:24 -0400

    remove logs

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -3,7 +3,6 @@ import axios from "axios";
 // const TUITS_API = 'https://tuiter-service.herokuapp.com/api/tuits/';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits/`;
-console.log(TUITS_API);
 
 
 
