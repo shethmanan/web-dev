d692db0aee3ba07d8c4bf4b2ed3fc7ef627090ea
shethmanan <shethmanan1997@gmail.com>
2022-04-06 22:14:06 -0400

    environment variable deployment setting

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-const TUITS_API = 'http://localhost:4000/api/tuits/';
+// const TUITS_API = 'http://localhost:4000/api/tuits';
+// const TUITS_API = 'https://tuiter-service.herokuapp.com/api/tuits/';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits/`;
+console.log(TUITS_API);
+
 
 
 export const createTuit = async (tuit) => {
