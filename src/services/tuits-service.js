97ffcd394afa98d4b60bd96d2e05d594b9842dfa
shethmanan <shethmanan1997@gmail.com>
2022-04-11 19:27:09 -0400

    point to new endpoint

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const TUITS_API = 'http://localhost:4000/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = process.env.REACT_APP_NEW_API_BASE || 'http://localhost:4000/api';
 const TUITS_API = `${API_BASE}/tuits/`;
 
 
